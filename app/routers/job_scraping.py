b48b9aa6ec512f4ef4f4cadbe91ab6f3c4b5fb45
Oscar Pairazaman <70730776+OscarEd08@users.noreply.github.com>
2024-11-13 00:07:38 -0500

    Se mejora el endpoint para la búsqueda de empleo

--- app/routers/job_scraping.py
+++ app/routers/job_scraping.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.services.job_scraping_service import get_jobs_in_json
 from app.shared.enums.job_site_type import JobSiteType
@@ -8,31 +8,34 @@ from app.shared.enums.job_type import JobType
 router = APIRouter()
 
 class JobRequest(BaseModel):
-    site_name: List[str] = [JobSiteType.indeed]
-    search_term: str = "Ingeniero de software"
-    google_search_term: str = "ingeniero de software cerca de Lima"
-    location: str = "Lima, Peru"
-    is_remote: bool = False
-    job_type: Optional[str] = JobType.fulltime
-    results_wanted: int = 10
-    country_indeed: str = "Peru"
-    offset: int = 0
-    hours_old: int = 72
-    enforce_annual_salary: bool = False
+    location: Optional[str] = None
+    google_search_term: Optional[str] = None
+    hours_old: Optional[int] = None
+    job_type: Optional[str] = None
+    is_remote: Optional[bool] = False
+    site_name: Optional[str] = JobSiteType.indeed
+    search_term: Optional[str] = "Ingeniero de software"
+    results_wanted: Optional[int] = 10
+    country_indeed: Optional[str] = "Peru"
+    offset: Optional[int] = 0
+    enforce_annual_salary: Optional[bool] = False
 
 @router.post("/jobs")
 async def get_jobs(job_query: JobRequest):
-    jobs_data = get_jobs_in_json(
-        site_name=job_query.site_name,
-        search_term=job_query.search_term,
-        google_search_term=job_query.google_search_term,
-        location=job_query.location,
-        is_remote=job_query.is_remote,
-        job_type=job_query.job_type,
-        results_wanted=job_query.results_wanted,
-        country_indeed=job_query.country_indeed,
-        offset=job_query.offset,
-        hours_old=job_query.hours_old,
-        enforce_annual_salary=job_query.enforce_annual_salary
-    )
-    return jobs_data
+    try:
+        jobs_data = get_jobs_in_json(
+            site_name=job_query.site_name,
+            search_term=job_query.search_term,
+            google_search_term=job_query.google_search_term,
+            location=job_query.location,
+            is_remote=job_query.is_remote,
+            job_type=job_query.job_type,
+            results_wanted=job_query.results_wanted,
+            country_indeed=job_query.country_indeed,
+            offset=job_query.offset,
+            hours_old=job_query.hours_old,
+            enforce_annual_salary=job_query.enforce_annual_salary
+        )
+        return jobs_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/services/job_scraping_service.py
+++ app/services/job_scraping_service.py
@@ -5,42 +5,67 @@ from jobspy import scrape_jobs
 from app.shared.enums.job_site_type import JobSiteType
 from app.shared.enums.job_type import JobType
 
+import json
+import logging
+
 def get_jobs_in_json(
-    site_name=[JobSiteType.indeed],
-    search_term="Ingeniero de software",
-    google_search_term="ingeniero de software cerca de Lima",
-    location="Lima, Peru",
-    is_remote=False,
-    job_type=JobType.fulltime,
-    results_wanted=10,
-    country_indeed="Peru",
-    offset=0,
-    hours_old=72,
-    enforce_annual_salary=False
+    location,
+    google_search_term,
+    hours_old,
+    job_type,
+    is_remote,
+    site_name,
+    search_term,
+    results_wanted,
+    country_indeed,
+    offset,
+    enforce_annual_salary
 ):
-    # Perform the scraping with dynamic parameters
-    df = scrape_jobs(
-        site_name=site_name,
-        search_term=search_term,
-        google_search_term=google_search_term,
-        location=location,
-        is_remote=is_remote,
-        job_type=job_type,
-        results_wanted=results_wanted,
-        country_indeed=country_indeed,
-        offset=offset,
-        hours_old=hours_old,
-        enforce_annual_salary=enforce_annual_salary,
-    )
-    
-    desired_columns = [
-        "id", "site", "job_url", "title", "company", "location",
-        "date_posted", "job_type", "interval", "min_amount",
-        "max_amount", "currency", "is_remote"
-    ]
-    
-    filtered_df = df[desired_columns]
-    jobs_json_str = filtered_df.to_json(orient='records')
-    jobs_json = json.loads(jobs_json_str)
-    
-    return jobs_json
+    try:
+        location = _setLocation(location)
+        google_search_term = _setGoogleSearchTerm(location, search_term)
+
+        # Perform the scraping with dynamic parameters
+        df = scrape_jobs(
+            site_name=site_name,
+            search_term=search_term,
+            google_search_term=google_search_term,
+            location=location,
+            is_remote=is_remote,
+            job_type=job_type,
+            results_wanted=results_wanted,
+            country_indeed=country_indeed,
+            offset=offset,
+            hours_old=hours_old,
+            enforce_annual_salary=enforce_annual_salary,
+        )
+
+        desired_columns = [
+            "id", "site", "job_url", "title", "company", "location",
+            "date_posted", "job_type", "interval", "min_amount",
+            "max_amount", "currency", "is_remote"
+        ]
+
+        if df.empty:
+            return []
+
+        filtered_df = df[desired_columns]
+        jobs_json_str = filtered_df.to_json(orient='records')
+        jobs_json = json.loads(jobs_json_str)
+
+        return jobs_json
+
+    except Exception as e:
+        logging.error(f"Error in get_jobs_in_json: {e}")
+        raise Exception(f"Error in get_jobs_in_json: {e}")
+        
+
+def _setLocation(location):
+    if location is None:
+        return "Lima, Peru"
+    return location + ", Peru"
+
+def _setGoogleSearchTerm(location, search_term):
+    if location is None:
+        return search_term + " cerca de Lima"
+    return search_term + " cerca de " + location
